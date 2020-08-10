71fb7589ccb1cd20f491aa0314834deaacb3f5e6
DorotaKw <dorotakwaskiewicz@gmail.com>
2020-08-10 16:03:01 +0200

    main

--- main.py
+++ main.py
@@ -48,13 +48,7 @@ def main():
     for animal in animals:
         print(animal.make_sound())
     cat.move()
-    manager = Manager('Jan', 'Kowalski', date(1992, 10, 10), 4500)
-    print(manager.salary)
-    manager.who_am_i()
-
-    employee = Employee('Jan', 'Kowalski', date(1992, 10, 10), 2500)
-    print(employee.salary)
-    employee.who_am_i()
+    
 
 if __name__ == "__main__":
     main()
