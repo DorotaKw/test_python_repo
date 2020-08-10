25bb40bb9e8df0475b4d74c7eaea16fb2c7eabe0
DorotaKw <dorotakwaskiewicz@gmail.com>
2020-08-10 15:57:49 +0200

    hello_world

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,14 @@
-def main():
-    print('Hello')
+from datetime import date
+from sda_ex_oop_2_mz.employee import Employee
+from sda_ex_oop_2_mz.manager import Manager
+
+    manager = Manager('Jan', 'Kowalski', date(1992, 10, 10), 4500)
+    print(manager.salary)
+    manager.who_am_i()
+
+    employee = Employee('Jan', 'Kowalski', date(1992, 10, 10), 2500)
+    print(employee.salary)
+    employee.who_am_i()
+
 if __name__ == "__main__":
-    main()
+
