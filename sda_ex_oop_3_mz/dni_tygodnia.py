32019a2e2fb23dc2f2908cf9b6e5b8db41c918de
DorotaKw <dorotakwaskiewicz@gmail.com>
2020-08-12 14:10:08 +0200

    Dni tygodnia

---
+++ sda_ex_oop_3_mz/dni_tygodnia.py
@@ -0,0 +1,9 @@
+
+dni_tygodnia = {1: "Monday", 2: "Tuesday", 3: "Wednesday",
+                     4: "Thursday", 5: "Friday", 6: "Saturday",
+                     7: "Sunday"}
+for key in dni_tygodnia:
+    print (key, dni_tygodnia[key])
+
+#print(dni_tygodnia[1])
+
