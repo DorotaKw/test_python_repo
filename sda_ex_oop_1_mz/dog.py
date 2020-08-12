4c49f498bfedf294c558b6e3edaaedb402293d83
DorotaKw <dorotakwaskiewicz@gmail.com>
2020-08-12 13:17:49 +0200

    Klasa ssak i psowate

--- sda_ex_oop_1_mz/dog.py
+++ sda_ex_oop_1_mz/dog.py
@@ -1,7 +1,9 @@
 from sda_ex_oop_1_mz.animal import Animal
+from sda_ex_oop_2_mz.canidae import Canidae
+from sda_ex_oop_2_mz.mammal import Mammal
 
 
-class Dog(Animal):
+class Dog(Animal, Mammal, Canidae):
     def __init__(self, name: str):
         super().__init__(name)
 
