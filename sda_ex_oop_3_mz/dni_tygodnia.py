618dc98296c09ed1b855530013d9bfcc46e230e1
DorotaKw <dorotakwaskiewicz@gmail.com>
2020-08-30 12:27:09 +0200

    oop3

--- sda_ex_oop_3_mz/dni_tygodnia.py
+++ sda_ex_oop_3_mz/dni_tygodnia.py
@@ -7,3 +7,18 @@ for key in dni_tygodnia:
 
 #print(dni_tygodnia[1])
 
+
+#def get_the_day_of_week(day_of_week: int) -> str:
+    #days = {
+     #   1: "Mon",
+    #   2: "Tue",
+      #  3: "Wed",
+       # 4: "Thu",
+     #   5: "Fri",
+     #  7: "Sun"
+ #   }
+  #  return days.get(day_of_week, "INCORRECT VALUE")
+#def main():
+   # print(get_the_day_of_week(1))
+#if __name__ == "__main__":
+ #   main()
